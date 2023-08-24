e20c02f0dd328c7487538cb011e39b607846e404
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-24 14:43:44 +0530

    update moduledemo

--- my-npm-project/module_demo.js
+++ my-npm-project/module_demo.js
@@ -2,5 +2,5 @@ let ascii_text_generator = require('ascii-text-generator');
 
 let input_text = "Hi Deshan Bandara";
 let ascii_text = ascii_text_generator(input_text, "3");
-
+r
 console.log(ascii_text);
