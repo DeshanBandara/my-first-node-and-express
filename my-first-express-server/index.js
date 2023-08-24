962f0ee22943c07c312a2de00f691daf5f6e9550
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-24 14:43:04 +0530

    added comment

--- my-first-express-server/index.js
+++ my-first-express-server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req,res)=>{
-ResizeObserver("Hello World");
+    ResizeObserver("Hello World");
 });
 
 app.listen(3000);
