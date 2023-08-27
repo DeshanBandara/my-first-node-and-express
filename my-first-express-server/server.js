3b136827fe4ea74893f0f1c80187e5eda829c0ec
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-27 12:38:16 +0530

    add server.js file

---
+++ my-first-express-server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = 8000;
+
+app.listen(HTTP_PORT, () =>{
+    console.log("Server Running on Port %PORT%". replace("%PORT%", HTTP_PORT));
+});
+
+app.get("/", (req, res)=>{
+    //Some code to retrive data
+    res.send("Getting Some Data");
+});
+
