265f7c0543a14b4418784538ab5ee2f4b15c3a7f
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-28 10:17:45 +0530

    added external.js file

---
+++ nodejs-Modules/external.js
@@ -0,0 +1,13 @@
+var details = {
+    level: function (level1) {
+        console.log('level1: ' + level1);
+    },
+    level2: function (level2) {
+        console.log('level2: ' + level2);
+    },
+    level3: function (level3) {
+        console.log('level3: ' + level3);
+    },
+};
+
+module.export = details;
